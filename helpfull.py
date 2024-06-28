133d0af2e617f0933fb27b3f815d745150d808f6
RockerBot <rockerbotmail@gmail.com>
2024-06-28 14:31:20 +0530

    handles elective and blocked slots

--- helpfull.py
+++ helpfull.py
@@ -1,23 +1,31 @@
-from states import CollapsedState, SuperState, Teacher, Subject, Section, Table_T
+from states import CollapsedState, SuperState, Teacher, Subject, Section, Table_T, GroupID_T, Group_T
 from colorama import Fore
 
-def print_tt(tt, dims, ndx=None):
+def print_tt(tt, dims, elective_slots, blocked_slots, ndx=None):
     n_sections, n_days_per_week, n_slots_per_day = dims
-    padding = 23#10  # len(''.join(subjects.keys()))+len(str(MAX_ENTROPY))+3-3
+    padding = 74#23#10  # len(''.join(subjects.keys()))+len(str(MAX_ENTROPY))+3-3
     if ndx is not None:
         print(ndx)
     for section_i in range(n_sections):
-        print('SECTION', section_i + 1)
+        print('\rSECTION', section_i + 1)
         for day_j in range(n_days_per_week):
-            print("day", day_j, end=':  ')
+            print("\r","-"*250,"\nday", day_j, end=':  ')
             for slot_k in range(n_slots_per_day):
                 state = tt[section_i][day_j][slot_k]
                 clr = ('\033[39m', '\033[92m')[isinstance(state, CollapsedState)]
-                if (section_i,day_j,slot_k) == ndx:
+                if blocked_slots[day_j][slot_k]:
+                    for i,elec in enumerate(elective_slots):
+                        if elec[day_j][slot_k]:
+                            print(f"{clr}{(f'elec{i+1}'.center(padding))}\033[39m", end=' ')
+                            break
+                    else:
+                        print(f"{clr}{'XXX'.center(padding)}\033[39m", end=' ')
+                elif (section_i,day_j,slot_k) == ndx:
                     print(f"{Fore.RED}{state:-{padding}}\033[39m", end=' ')
                 else:
                     print(f"{clr}{state:-{padding}}\033[39m", end=' ')
-            print()
+                if slot_k%3==2:
+                    print("\n        ",end='')
 
 def print_tt_stats(tt, dims, groupings, block_subjects):
     n_sections, n_days_per_week, n_slots_per_day = dims
@@ -36,12 +44,16 @@ def print_tt_stats(tt, dims, groupings, block_subjects):
         print()
 
 
-def score_faculty(blocked_slots, dims, teachers:list[Teacher], sections:list[Section]):
+def score_faculty(blocked_slots, elective_slots, dims, teachers:list[Teacher], sections:list[Section]):
     n_sections, n_days_per_week, n_slots_per_day = dims
 
-    for i in n_days_per_week:
-        for j in n_slots_per_day:
-            if not blocked_slots[i][j]: continue
+    for i in range(n_days_per_week):
+        for j in range(n_slots_per_day):
+            for elec in elective_slots:
+                if elec[i][j]: break
+            else: 
+                if not blocked_slots[i][j]:
+                    continue
             for teacher in teachers:
                 teacher.availability[i][j] = 1
             for section in sections:
@@ -63,8 +75,8 @@ def create_groupings(teachers:list[Teacher], groupings):
             subj.groupIDs |= {key}
     groupings[(0,0)] = 0
 
-def setup(blocked_slots, dims, teachers:list[Teacher], sections, subjects, groupings) -> Table_T:
-    score_faculty(blocked_slots, dims, teachers, sections)
+def setup(dims, blocked_slots, elective_slots, teachers:list[Teacher], sections, subjects, groupings:dict[GroupID_T, Group_T]) -> Table_T:
+    score_faculty(blocked_slots, elective_slots, dims, teachers, sections)
     create_groupings(teachers, groupings)
     n_sections, n_days_per_week, n_slots_per_day = dims
     table: Table_T = [
@@ -80,5 +92,13 @@ def setup(blocked_slots, dims, teachers:list[Teacher], sections, subjects, group
             for slot_k in range(n_slots_per_day)]
         for day_j in range(n_days_per_week)]
     for section_i in range(n_sections)]
+    
+    for j in range(n_days_per_week):
+        for k in range(n_slots_per_day):
+            for elec in elective_slots:
+                if not elec[j][k]: continue
+                for i in range(n_sections):
+                    table[i][j][k] = CollapsedState((0,0))#TODO collapsed states with elective subj
+
     return table
     
